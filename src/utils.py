9a2af57c2408363cae62df32d63600251de2d69a
husticfilip <hustic.filip@gmail.com>
2023-10-21 13:36:13 +0200

    doraden open_html

--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,33 @@
 import re
+import copy
 import requests
+from urllib.parse import urlparse
 import logging
 
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0',
            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-           'Accept-Language': '*',
-           'Accept-Encoding': 'gzip, deflate, br',
+           'Accept-Language': 'en-US,en;q=0.5',
+           'Accept-Encoding': 'gzip, deflate',
            'Referer': 'https://www.google.com/'}
 
 
+def add_host_to_headers(url: str):
+    new_headers = copy.copy(headers)
+    host = urlparse(url).netloc
+
+    if not host.startswith("www."):
+        host = "www." + host
+    new_headers["Host"] = host
+    return new_headers
+
 def open_html(url: str):
     response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        logging.info("Couldn't access website, adding 'Host' to the header")
+        new_headers = add_host_to_headers(url)
+        response = requests.get(url, headers=new_headers)
+
+    print(response.headers)
     return response.text, response.status_code
 
 def format_logo_path(logo_path: str, url: str):
