014d90810cab0a295f41c522f80da4bcb7af8700
husticfilip <hustic.filip@gmail.com>
2023-10-21 13:36:33 +0200

    reformatiran extract.py

--- src/extract.py
+++ src/extract.py
@@ -1,26 +1,11 @@
 from src.extractors.phone.phoneNumberExtractor import PhoneNumberExtractor, FilterEnum
-import requests
 from pathlib import Path
 from src.extractors.logo.logoExtractor import LogoExtractor
 import logging
 import sys
 from src.extractors.beautifulSoupWrapper import BeautifulSoupHtmlWrapper
-import re
-
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0',
-           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-           'Accept-Language': '*',
-           'Accept-Encoding': 'gzip, deflate, br',
-           'Referer': 'https://www.google.com/'}
-
-
-# TODO - make checks if request passed, check if url is valid
-def get_html(url):
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return response.text
-    else:
-        raise Exception("Error reading html")
+from utils import print_out_result, open_html
+import traceback
 
 
 # TODO - makni
@@ -28,37 +13,37 @@ def readFromTheFile(name):
     return Path("resources/" + name).read_text()
 
 
-def format_phone_numbers(phone_numbers: list[str]):
-    if len(phone_numbers) == 0:
-        return None
-    phone_numbers_set = set()
-    for number in phone_numbers:
-        phone_numbers_set.add(re.sub("[^\d+\(\)]", " ", number.strip()))
-    return phone_numbers_set
-
-
-def format_logo_path(logo_path: str, url: str):
-    if logo_path == "":
-        return logo_path
-    if logo_path.startswith("http"):
-        return logo_path
-    else:
-        url = re.sub(r'\\', '/', url)
-        logo_path = re.sub(r'\\', '/', logo_path)
-
-        if url.endswith("/") and logo_path.startswith("/"):
-            return url + logo_path[1:]
-        elif not url.endswith("/") and not logo_path.startswith("/"):
-            return url + "/" + logo_path
-        else:
-            return url + logo_path
-
-
 def configure_logs(LOG_LEVEL):
     logging.basicConfig(level=LOG_LEVEL,
                         format='[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s: %(message)s',
                         datefmt='%m/%d/%Y %H:%M:%S',
                         stream=sys.stderr)
+
+
+def get_user_url_input():
+    if len(sys.argv) == 1:
+        print("Please provide url to the website as program argument.")
+        exit()
+    if len(sys.argv) > 2:
+        print("Please provide only one program argument which is url to the website.")
+        exit()
+    else:
+        return sys.argv[1]
+
+
+def get_html():
+    logging.info("Opening url " + url)
+    try:
+        html, status_code = open_html(url)
+        if status_code != 200:
+            print("Could not open website, got status code " + status_code + ".")
+            exit()
+        else:
+            return html
+    except Exception as ex:
+        logging.error(ex, exc_info=True)
+        print("Could not open website. Please check that url is correct.")
+        exit()
 
 
 def init_phone_number_extractor(beautiful_soup_wrapper):
@@ -72,39 +57,25 @@ def init_phone_number_extractor(beautiful_soup_wrapper):
     return numberExtractor
 
 
-def print_out_result(phone_numbers, logo_path):
-    formated_numbers = format_phone_numbers(phone_numbers)
-    formated_logo_path = format_logo_path(logo_path, url)
-
-    if formated_numbers is None:
-        print("None")
-    else:
-        result_string = ""
-        for number in formated_numbers:
-            result_string += number + ", "
-        result_string = result_string[0:-2]
-        print(result_string )
-
-    if formated_logo_path == "":
-        print("None")
-    else:
-        print(formated_logo_path)
-
 if __name__ == '__main__':
-    url = "phosagro"
     LOG_LEVEL = logging.INFO
     configure_logs(LOG_LEVEL)
 
-    # html = get_html("https://www.cmsenergy.com/contact-us/default.aspx")
-    #html = readFromTheFile("phosagro.txt")
-    html = " "
-    beautiful_soup_wrapper = BeautifulSoupHtmlWrapper(html)
+    url = get_user_url_input()
+    html = get_html()
+    #html = readFromTheFile("cial.txt")
 
-    numberExtractor = init_phone_number_extractor(beautiful_soup_wrapper)
-    numbers = numberExtractor.extractNumbers()
+    try:
+        logging.info("Initializing BeautifulSoupHtmlWrapper and parsing html with BeautifulSoup html.parser")
+        beautiful_soup_wrapper = BeautifulSoupHtmlWrapper(html)
 
-    logoExtractor = LogoExtractor(beautiful_soup_wrapper)
-    logo_path = logoExtractor.extract()
+        numberExtractor = init_phone_number_extractor(beautiful_soup_wrapper)
+        numbers = numberExtractor.extractNumbers()
 
-    print_out_result(numbers, logo_path)
-
+        logoExtractor = LogoExtractor(beautiful_soup_wrapper)
+        logo_path = logoExtractor.extract()
+        print_out_result(numbers, logo_path, url)
+    except Exception as ex:
+        print("Problem while running number and logo extractors. Please check out the logs.")
+        logging.error("Exception raised while running number and logo extractors")
+        logging.error(ex, exc_info=True)
