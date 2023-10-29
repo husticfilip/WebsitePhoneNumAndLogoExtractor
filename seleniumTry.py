ef1af80c29f610c9ad690803de9bfdb743951c5b
husticfilip <hustic.filip@gmail.com>
2023-10-29 20:49:44 +0100

    obrisani nepotrebni file-ovi

--- seleniumTry.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
